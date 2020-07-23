d67b042c9e1504023b974c51ddc8d8a9790cc52a
sam <samchim205@gmail.com>
2020-07-23 10:54:19 +0800

    preprocess.py fit col[1]

--- datasets/preprocess.py
+++ datasets/preprocess.py
@@ -219,7 +219,7 @@ def mytype(root_path, meta_file):
         for line in ttf:
             cols = line.split('|')
             wav_file = os.path.join(root_path, 'wavs', cols[0] + '.wav')
-            text = cols[1]
+            text = cols[2]
             speaker_name = "Oscar"
             items.append([text, wav_file, speaker_name])
     return items
